64b0e06fd5048afcb104c3d4ca9aa08ff8af149a
Danial Kama <DanialKamali777@gmail.com>
2022-02-01 22:56:55 +0330

    New anim notify classes.
    New anim notify classes from Third-Person Shooter project added.
    https://github.com/DanialKama/ThirdPersonShooter.git

---
+++ Source/TPSOnline/Private/Animations/AnimNotify_Ragdoll.cpp
@@ -0,0 +1,16 @@
+// Copyright 2022 Danial Kamali. All Rights Reserved.
+
+#include "Animations/AnimNotify_Ragdoll.h"
+#include "Characters/BaseCharacter.h"
+
+void UAnimNotify_Ragdoll::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (MeshComp && MeshComp->GetOwner())
+	{
+		ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner());
+		if (Character)
+		{
+			// Character->DeathMontageHandler(nullptr, false);
+		}
+	}
+}
